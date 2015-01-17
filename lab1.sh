3a02001d2a061ea521075759dabb6ba50d4753bb
Tim Rayburn <tim@timrayburn.net>
2015-01-16 23:28:13 -0600

    changing lab numbers

---
+++ lab1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf ./lab1
+mkdir ./lab1
+cd ./lab1
+git init
---
+++ lab2.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+rm -rf ./lab2
+mkdir ./lab2
+cd ./lab2
+git init
+
+# Setup one commit on master
+touch README
+git add .
+git commit -am "First Commit"
+
+# branch to working
+git checkout -b working
+
+# do work & commit
+touch file1.txt
+git add .
+git commit -am "working commit1"
+touch file2.txt
+git add .
+git commit -am "working commit2"
+touch file3.txt
+git add .
+git commit -am "working commit3"
+
+# back to master
+git checkout master
+
+# branch to feature
+git checkout -b feature
+
+# do work & commit
+echo "Hello World" > file1.txt
+git add .
+git commit -am "bugfix commit1"
+echo "Hello World" > file2.txt
+git add .
+git commit -am "bugfix commit2"
+echo "Hello World" > file3.txt
+git add .
+git commit -am "bugfix commit3"
+
+# back to master
+git checkout master
+
+clear
